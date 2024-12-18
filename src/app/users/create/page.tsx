44783e1271b509bfce24f70694b61499ae94127b
bernardotonin <btprates2004@gmail.com>
2024-12-17 21:59:29 -0300

    feat: create create user page

---
+++ src/app/users/create/page.tsx
@@ -0,0 +1,6 @@
+import UserCreatePage from "@/pages/UserCreatePage";
+import React from "react";
+
+export default function UserCreate() {
+  return <UserCreatePage />;
+}
---
+++ src/pages/UserCreatePage.tsx
@@ -0,0 +1,10 @@
+import { CreateUserForm } from "@/components/UserDashboard/CreateUserForm";
+
+export default function UserCreatePage() {
+  return (
+    <div className="flex items-center justify-center h-[90vh] font-[family-name:var(--font-geist-sans)]">
+      
+      <CreateUserForm />
+    </div>
+  );
+}
